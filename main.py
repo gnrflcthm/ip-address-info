c0fbb9bce4248cc134a7f564b5d8e71114f8b135
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-09-26 20:37:13 +0800

    removed unused and redundant methods

--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-import socket
 from typing import Dict
 from requests import get
-import re
 import json
 import sys
 
 """API Used for getting ip address information"""
-IP_API_URL = "https://ipapi.co/{}/json/"
-
-"""Public site for retrieving machine's public ip address"""
-PUBLIC_IP_API_URL = "https://www.ipchicken.com/"
-
-"""Gets the machine's local IP Address"""
-def get_local_ip_address() -> str:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-
+IP_API_URL = "https://ipapi.co"
 
 """Gets the ip address information on the given public IP Address"""
-def get_ip_adress_info(address="0.0.0.0") -> Dict:
-    res = get(IP_API_URL.format(address))
+def get_ip_adress_info(address=None) -> Dict:
+    res = get(f'{IP_API_URL}/{address}/json') if address else get(f'{IP_API_URL}/json')
     if res.text == None:
         raise Exception("An Error has occured fetching IP Address Information")
     """Returns IP Address Information as a Python Dictionary"""
     return json.loads(res.text)
 
-
-"""Gets the machine's public IP address"""
-def get_public_ip_address() -> str:
-    res = get(PUBLIC_IP_API_URL)
-    try:
-        """Makes use of RegEx to match the pattern resembling an IP Address"""
-        return re.findall(r"\d+\.\d+\.\d+\.\d+", res.text)[0]
-    except:
-        raise Exception("An Error has occured.")
-
+"""Formats output returned from the API"""
 def print_formatted(title: str, data: Dict) -> None:
     print(f"\n{title.title()}\n")
     for key, val in data.items():
@@ -45,7 +24,7 @@ def print_formatted(title: str, data: Dict) -> None:
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) == 0:
-        data = get_ip_adress_info(get_public_ip_address())
+        data = get_ip_adress_info()
         print_formatted("IP Address Information", data)
     else:
         address = args[0]
